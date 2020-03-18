66cef8510c5cd6ad6d42d67842cde8924c694535
Shaw Coney <rockconey@gmail.com>
2020-03-17 22:54:02 -0500

    beginning of code for assignment 5

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-// Write your JavaScript code here!
+window.addEventListener("load", function(){
+   let form = document.querySelector("form");
+
+   form.addEventListener("submit", function(event) {
+      let piName = document.getElementById("pilotName");
+      let coPiName = document.querySelector("input[name=copilotName]");
+      let fuel = document.querySelector("input[name=fuelLevel");
+      let mass = document.querySelector("input[name=cargoMass");
+
+      if (piName.value === "" || coPiName.value === "" || fuel.value === "" || mass.value === "") {
+         alert("All Fields Required");
+         event.preventDefault();
+      }
+
+      if (isNaN(piName.value) || isNan(coPiName.value)) {
+         
+      }
+   })
+})
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
