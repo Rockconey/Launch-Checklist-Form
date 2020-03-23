17e58797660007a92f40757bfeec9e07052a0c98
Shaw Coney <rockconey@gmail.com>
2020-03-23 17:00:37 -0500

    complete with bonus mission

--- script.js
+++ script.js
@@ -1,112 +1,91 @@
 window.addEventListener("load", function(){
+
    let form = document.querySelector("form");
+
    fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
         response.json().then(function(json) {
           
-           console.log(json[5]);
-           let jsonObj = json;
-
+           const random = document.getElementById("random");
            const div = document.getElementById("missionTarget");
-         //   let randInd =   
-         //   jsonObj[Math.floor(Math.random() * arr.length)];
 
-           div.innerHTML = 
+           random.addEventListener("click", function() {
 
-            `<h2>Mission Destination</h2>
-           <ol>
-               <li>Name: ${jsonObj[5].name}</li>
-               <li>Diameter: ${jsonObj[5].diameter}</li>
-               <li>Star: ${jsonObj[5].star}</li>
-               <li>Distance from Earth: ${jsonObj[5].distance}</li>
-               <li>Number of Moons: ${jsonObj[5].moons}</li>           
-           </ol>
-           <img src="${jsonObj[5].image}">
-           <br>
-           <button id="random">Choose Random Destination</button>`
-           ;
+               let randInd = json[Math.floor(Math.random() * json.length)];
 
-         /* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
+               div.innerHTML = 
+
+                `<h2>Mission Destination</h2>
+               <ol>
+                  <li>Name: ${randInd.name}</li>
+                  <li>Diameter: ${randInd.diameter}</li>
+                  <li>Star: ${randInd.star}</li>
+                  <li>Distance from Earth: ${randInd.distance}</li>
+                  <li>Number of Moons: ${randInd.moons}</li>           
+               </ol>
+               <img src="${randInd.image}">
+               <br>`;
+            })
+           
             
 
-   form.addEventListener("submit", function(event) {
-      let piName = document.getElementById("pilotName");
-      let coPiName = document.querySelector("input[name=copilotName]");
-      let fuel = document.querySelector("input[name=fuelLevel");
-      let mass = document.querySelector("input[name=cargoMass");
+      form.addEventListener("submit", function(event) {
+         let piName = document.getElementById("pilotName");
+         let coPiName = document.querySelector("input[name=copilotName]");
+         let fuel = document.querySelector("input[name=fuelLevel");
+         let mass = document.querySelector("input[name=cargoMass");
 
-      let updateInfo = document.getElementById("faultyItems");
-      let readyPilot = document.getElementById("pilotStatus");
-      let readyCoPilot = document.getElementById("copilotStatus");
-      let fuelStat = document.getElementById("fuelStatus");
-      let launchStat = document.getElementById("launchStatus");
-
-      
-
-      if (piName.value === "" || coPiName.value === "" || fuel.value === "" || mass.value === "") {
-         alert("All Fields Required");
-         event.preventDefault();
-      }
+         let updateInfo = document.getElementById("faultyItems");
+         let readyPilot = document.getElementById("pilotStatus");
+         let readyCoPilot = document.getElementById("copilotStatus");
+         let fuelStat = document.getElementById("fuelStatus");
+         let launchStat = document.getElementById("launchStatus");
+         let cargoStat = document.getElementById("cargoStatus")
 
       
 
-      if (!isNaN(coPiName.value) || !isNaN(piName.value)) {
-         alert("Invalid Input.");
-         event.preventDefault();
-      } 
+         if (piName.value === "" || coPiName.value === "" || fuel.value === "" || mass.value === "") {
+            alert("All Fields Required");
+            event.preventDefault();
+         }
 
-      if (isNaN(fuel.value) || (isNaN(mass.value))) {
-         alert("Invalid Input.");
-         event.preventDefault();
+      
+
+         if (!isNaN(coPiName.value) || !isNaN(piName.value)) {
+            alert("Invalid Input.");
+            event.preventDefault();
+         } 
+
+         if (isNaN(fuel.value) || (isNaN(mass.value))) {
+            alert("Invalid Input.");
+            event.preventDefault();
         
-      }
+         }
 
-      if (fuel.value < 10000) {
-         event.preventDefault();
-         
-         document.getElementById("fuelStatus").innerHTML = "There is not enough fuel for the journey."
-        
-         document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch."
-         document.getElementById("launchStatus").style.color = "red";
-         
-         document.getElementById("faultyItems").style.visibility = "visible";
-      // } else {
-      //    document.getElementById("launchStatus").innerHTML = "Shuttle is ready for launch.";
-      //    document.getElementById("launchStatus").style.color = "green";
-      //    event.preventDefault();
-      }
+         if (fuel.value < 10000) {
+            event.preventDefault();
+            fuelStat.innerHTML = "There is not enough fuel for the journey.";        
+            launchStat.innerHTML = "Shuttle not ready for launch.";
+            launchStat.style.color = "red";
+            updateInfo.style.visibility = "visible";        
+         }
 
-      if (mass.value > 10000) {
-         event.preventDefault();
-         document.getElementById("faultyItems").style.visibility = "visible";
-         document.getElementById("cargoStatus").innerHTML = "There is too much mass for the shuttle to take off."
-         document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch."
-         document.getElementById("launchStatus").style.color = "red";
+         if (mass.value > 10000) {
+            event.preventDefault();
+            launchStat.innerHTML = "Shuttle not ready for launch.";
+            launchStat.style.color = "red";
+            updateInfo.style.visibility = "visible";
+            cargoStat.innerHTML = "There is too much mass for the shuttle to take off."
+         }
 
-      // }else {
-      //    document.getElementById("launchStatus").innerHTML = "Shuttle is ready for launch.";
-      //    document.getElementById("launchStatus").style.color = "green";
-      //    event.preventDefault();
-      }
-
-      if (isNaN(coPiName.value) && isNaN(piName.value) && !isNaN(fuel.value) && !isNaN(mass.value) && fuel.value >=10000 && mass.value <= 10000) {
-         document.getElementById("launchStatus").innerHTML = "Shuttle is ready for launch.";
-         document.getElementById("launchStatus").style.color = "green";
-      }
+         if (isNaN(coPiName.value) && isNaN(piName.value) && !isNaN(fuel.value) && !isNaN(mass.value) && fuel.value >=10000 && mass.value <= 10000) {
+            launchStat.innerHTML = "Shuttle is ready for launch"
+            launchStat.style.color = "green";
+         }
 
       
 
-      readyPilot.innerHTML = (`${piName.value} Ready`);
-      readyCoPilot.innerHTML = (`${coPiName.value} Ready`);
+         readyPilot.innerHTML = (`${piName.value} Ready`);
+         readyCoPilot.innerHTML = (`${coPiName.value} Ready`);
       
       
       
